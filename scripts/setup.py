f1c2d3467ca393c64a543d129396f30a4338bc57
Joon Young Lee <joonyoung.lee@duke.edu>
2021-10-05 23:52:32 -0400

    added docker compose setup scripts

---
+++ scripts/setup.py
@@ -0,0 +1,17 @@
+import os 
+import subprocess 
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(CURR_DIR)
+
+TIME_SERIES_DIR = os.path.join(BASE_DIR, 'timeseriesdb')
+TIME_SERIES_SCRIPTS = os.path.join(TIME_SERIES_DIR, 'scripts')
+
+PYTHON = 'python3'
+
+def main():
+    subprocess.call(os.path.join(CURR_DIR, "setup.sh"))
+    subprocess.call([PYTHON, os.path.join(TIME_SERIES_SCRIPTS, 'dbsetup.py')])
+
+if __name__ == "__main__":
+    main()
